316322cbc2f052a6d8f7d99a76aa34804cf1f311
ttuluk <ttasya.k@gmail.com>
2021-09-03 22:17:19 +0300

    исправляет изменение темы

---
+++ src/js/theme.js
@@ -0,0 +1,44 @@
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeLightClass = Theme.LIGHT;
+const themeDarkClass = Theme.DARK;
+
+const STORAGE_KEY = 'check';
+
+const bodyEl = document.querySelector('body');
+const inputThemeSwitch = document.querySelector('#theme-switch-toggle');
+
+console.dir(inputThemeSwitch);
+inputThemeSwitch.addEventListener('change', changeTheme);
+
+
+function changeTheme(evt) {
+  const checked = evt.target.checked;
+
+    if (checked === true) {
+      bodyEl.classList.add(themeDarkClass);
+      bodyEl.classList.remove(themeLightClass);
+      localStorage.setItem(STORAGE_KEY, checked)
+    }
+
+    if (checked === false) {
+
+      bodyEl.classList.replace(themeDarkClass, themeLightClass);
+      localStorage.setItem(STORAGE_KEY, checked);
+  }
+};
+
+function populateInput() {
+    const lo = localStorage.getItem(STORAGE_KEY);
+
+    if (lo === 'true') {
+    inputThemeSwitch.checked = true;
+        bodyEl.classList.add(themeDarkClass);
+    }
+localStorage.removeItem(STORAGE_KEY);
+}
+populateInput();
