15b599845786d43649bd1504ff1a9742a7d56796
ttuluk <ttasya.k@gmail.com>
2021-09-03 07:50:53 +0300

    finish theme

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -3,40 +3,24 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-// localStorage.setItem('theme', JSON.stringify(Theme));
-// const saveData = localStorage.getItem('theme');
-// const parseData = JSON.parse(saveData);
-// console.log(parseData);
-
+const themeLightClass = Theme.LIGHT;
+const themeDarkClass = Theme.DARK;
 
 const bodyEl = document.querySelector('body');
 const themeSwitchEl = document.querySelector('#theme-switch-toggle');
 
-console.log(bodyEl);
-console.log(themeSwitchEl);
-
 themeSwitchEl.addEventListener('change', addClassTheme);
-const themeLightClass = Theme.LIGHT;
-const themeDarkClass = Theme.DARK;
-
-const classOfBody = bodyEl.className;
-// console.log(classOfBody);
-
-// const defaultTheme = bodyEl.classList.add(themeLightClass);
 
 function addClassTheme(evt) {
-
+  evt.preventDefault();
+  if (themeSwitchEl.checked === true) {
     bodyEl.classList.add(themeDarkClass);
     bodyEl.classList.remove(themeLightClass);
     localStorage.setItem('theme', themeDarkClass)
-    themeSwitchEl.addEventListener('change', removeClassTheme);
-
-
-};
-function removeClassTheme(evt) {
+  }
+  if (themeSwitchEl.checked === false) {
     bodyEl.classList.replace(themeDarkClass, themeLightClass);
     localStorage.removeItem('theme');
-  localStorage.setItem('theme', themeLightClass);
-
+    localStorage.setItem('theme', themeLightClass);
+  }
 };
-
